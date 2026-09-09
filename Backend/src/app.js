4a23ff76617b6625851adad3224c8a8e9e257955
Aisha-Jawed <aishajawed2285@gmail.com>
2026-09-09 18:50:02 +0530

    Backend

---
+++ Backend/src/app.js
@@ -0,0 +1,15 @@
+const express=require('express')
+const cookieParser=require('cookie-parser')
+const app=express()
+
+app.use(express.json())
+
+/* require all the routes herev*/
+const authRouter=require('./routes/auth.routes')
+
+/* using all the routes here*/
+app.use('/api/auth',authRouter)
+
+
+
+module.exports=app
---
+++ Backend/src/routes/auth.routes.js
@@ -0,0 +1,36 @@
+const express= require('express')
+const authController=require('../controllers/auth.controller')
+const authMiddleWare=require('../middlewares/auth.middleware')
+const authRouter=express.Router()
+/**
+ * @route POST /api/auth/register
+ * @description Register a new user
+ * @access Public
+ */
+authRouter.post('/register', authController.registerUserController)
+
+/**
+ * @route POST /api/auth/login
+ * @description login user with email and password
+ * @access Public
+*/
+
+authRouter.post("/login", authController.loginUserController)
+
+/**
+ * @route GET /api/auth/logout
+ * @description clear token from the cookie and add the token in blacklist
+ * @access Public
+*/
+authRouter.post("/logout", authController.logoutUserController)
+
+/**
+ * @route GET/api/auth/get-me
+ * @description get the current logged in user details
+ * @access private
+ */
+
+authRouter.get("/get-me", authMiddleWare.authUser, authController.getMeController)
+
+
+module.exports=authRouter
